1b7fb44eb42e624f5666516b69f82020da1a4faa
Danrothcrawl <dquitze@gmail.com>
2025-04-28 15:03:01 +0000

    date range

--- .learn/resets/04-Series/app.py
+++ .learn/resets/04-Series/app.py
@@ -3,6 +3,7 @@
 
 # data_frame = pd.read_csv('.learn/assets/pokemon_data.csv')
 # print(data_frame)
+list_of_ages = [23,45,7,34,6,63,36,78,54,34]
 import pandas as pd
-ages = pd.Series([23,45,7,34,6,63,36,78,54,34])
-print(ages)
+data = pd.Series([list_of_ages])
+print(data)
