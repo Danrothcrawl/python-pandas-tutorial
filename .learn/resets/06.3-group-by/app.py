2e7a3f9fc2211363450290e0f33609d760300ad7
Danrothcrawl <dquitze@gmail.com>
2025-04-28 20:35:44 +0000

    Finish

--- .learn/resets/06.3-group-by/app.py
+++ .learn/resets/06.3-group-by/app.py
@@ -83,6 +83,5 @@
 import pandas as pd
 
 data_frame = pd.read_csv('.learn/assets/us_baby_names_right.csv')
-names = data_frame.groupby["Name"].sum()
-
-print(len(names))
+names = data_frame['Name'].groupby()
+print(names)
